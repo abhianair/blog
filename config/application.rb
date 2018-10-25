9e93bd5345416cf47fc8a27386e44271f7af1ad2
abhishek aravindan <abhishekaravindan@gmail.com>
2018-10-25 12:55:12 +0530

    changes

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,5 @@
 require_relative 'boot'
-config.assets.initialize_on_precompile = false
+
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module Blog
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
